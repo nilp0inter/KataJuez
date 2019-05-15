10be377b3fec19bd2e188ba15347186b466e9255
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-05-15 13:03:03 +0200

    Comments and refactor.

--- juez.c
+++ juez.c
@@ -2,6 +2,7 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
@@ -12,36 +13,51 @@
 #define EXIT_DIFF 1
 #define EXIT_FAIL 255
 
+#define eexit(msg) perror(msg); return EXIT_FAIL;
+
 
 int main(int argc, char *argv[]) {
-	if (argc != 3) return 255;	
+	if (argc != 3) {
+		fprintf(stderr, "%s <file1> <file2>\n", argv[0]);
+		return EXIT_FAIL;
+	}
 
-	int fh1 = open(argv[1], O_RDONLY, 0);
-	int fh2 = open(argv[2], O_RDONLY, 0);
-	if (fh1 < 0 || fh2 < 0) return EXIT_FAIL;
+	int fh1 = open(argv[1], O_RDWR, 0);
+	if (fh1 < 0) { eexit("Cannot open first file"); }
 
-	// Files differ in size?
+	int fh2 = open(argv[2], O_RDWR, 0);
+	if (fh2 < 0) { eexit("Cannot open second file"); }
+
+	
+	/**************************************************************
+	*                  Do files differ in size?                  *
+	**************************************************************/
 	long f1s = lseek(fh1, 0L, SEEK_END);
 	long f2s = lseek(fh2, 0L, SEEK_END);
 	if (f1s != f2s) return EXIT_DIFF;
 
-	// Files differ in content?
-	void *f1 = mmap(NULL, f1s, PROT_READ, MAP_PRIVATE, fh1, 0);
-	void *f2 = mmap(NULL, f2s, PROT_READ, MAP_PRIVATE, fh2, 0);
-	if (f1 == MAP_FAILED || f2 == MAP_FAILED) {
-		printf("Can't mmap!\n");
-		return EXIT_FAIL;
-	}
 
+	/**************************************************************
+	*                Do files differ in content?                 *
+	**************************************************************/
+	void *f1 = mmap(NULL, f1s, PROT_READ, MAP_PRIVATE, fh1, 0);
+	if (f1 == MAP_FAILED) { eexit("Cannot map first file"); }
+
+	void *f2 = mmap(NULL, f2s, PROT_READ, MAP_PRIVATE, fh2, 0);
+	if (f2 == MAP_FAILED) { eexit("Cannot map second file"); }
+
+	// One thread per processor core and one file chunk per thread
 	omp_set_num_threads(omp_get_num_procs());
 	size_t chunk = f1s/omp_get_num_procs();
 
 	#pragma omp parallel for
 	for (size_t s=0; s<f1s; s+=chunk) {
 		int offset = s+chunk > f1s ? chunk-((s+chunk)-f1s) : chunk;
+
+		// Force exit if the current chunk differs
 		if (memcmp(f1+s, f2+s, offset) != 0) _exit(EXIT_DIFF);
 	}
 
-	// Files are equal
+	// Both files are equal!
 	return EXIT_EQUAL;
 }
