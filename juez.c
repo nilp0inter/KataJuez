ec2553219885ab4fac6dd77264797ee888d26af1
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-05-15 19:47:12 +0200

    Use stat to compare file sizes.

--- juez.c
+++ juez.c
@@ -22,37 +22,41 @@ int main(int argc, char *argv[]) {
 		return EXIT_FAIL;
 	}
 
-	int fh1 = open(argv[1], O_RDWR, 0);
-	if (fh1 < 0) { eexit("Cannot open first file"); }
-
-	int fh2 = open(argv[2], O_RDWR, 0);
-	if (fh2 < 0) { eexit("Cannot open second file"); }
-
-	
 	/**************************************************************
 	*                  Do files differ in size?                  *
 	**************************************************************/
-	long f1s = lseek(fh1, 0L, SEEK_END);
-	long f2s = lseek(fh2, 0L, SEEK_END);
-	if (f1s != f2s) return EXIT_DIFF;
+	unsigned long size;
+	{
+		struct stat f1s;
+		struct stat f2s;
 
+		if (stat(argv[1], &f1s) < 0) { eexit("Cannot stat first file"); }
+		if (stat(argv[2], &f2s) < 0) { eexit("Cannot stat second file"); }
+
+		if (f1s.st_size != f2s.st_size) return EXIT_DIFF;
+		size = f1s.st_size;
+	}
 
 	/**************************************************************
 	*                Do files differ in content?                 *
 	**************************************************************/
-	void *f1 = mmap(NULL, f1s, PROT_READ, MAP_PRIVATE, fh1, 0);
+	int fh1 = open(argv[1], O_RDWR, 0);
+	if (fh1 < 0) { eexit("Cannot open first file"); }
+	void *f1 = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fh1, 0);
 	if (f1 == MAP_FAILED) { eexit("Cannot map first file"); }
 
-	void *f2 = mmap(NULL, f2s, PROT_READ, MAP_PRIVATE, fh2, 0);
+	int fh2 = open(argv[2], O_RDWR, 0);
+	if (fh2 < 0) { eexit("Cannot open second file"); }
+	void *f2 = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fh2, 0);
 	if (f2 == MAP_FAILED) { eexit("Cannot map second file"); }
 
 	// One thread per processor core and one file chunk per thread
 	omp_set_num_threads(omp_get_num_procs());
-	size_t chunk = f1s/omp_get_num_procs();
+	size_t chunk = size/omp_get_num_procs();
 
 	#pragma omp parallel for
-	for (size_t s=0; s<f1s; s+=chunk) {
-		int offset = s+chunk > f1s ? chunk-((s+chunk)-f1s) : chunk;
+	for (size_t s=0; s<size; s+=chunk) {
+		int offset = s+chunk > size ? chunk-((s+chunk)-size) : chunk;
 
 		// Force exit if the current chunk differs
 		if (memcmp(f1+s, f2+s, offset) != 0) _exit(EXIT_DIFF);
